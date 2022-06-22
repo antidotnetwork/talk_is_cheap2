5bcfdca095e01265c408c2d286926b282a25899c
anti-dot-net <107893809+anti-dot-net@users.noreply.github.com>
2022-06-22 14:04:36 -0400

    Update ticb.py

--- ticb.py
+++ ticb.py
@@ -1,26 +1,43 @@
 from mastodon import Mastodon
-
+from bs4 import BeautifulSoup
+import re
 
 mastodon = Mastodon(
   access_token = 'token.secret',
   api_base_url = 'https://tavern.antinet.work'
 )
+CLEANR = re.compile('<.*?>') 
+
+def h2t(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+  return cleantext
+  
+botself = mastodon.me()
+botself_id = botself["id"]
 
 def fetch_notifications():
-	botself = mastodon.me()
-	botself_id = botself["id"]
 	print (botself_id)
 	statuses = mastodon.notifications(limit=1)
 	status_dict = statuses[0]
 	status_type = status_dict['type']
+	status_id = status_dict['id']
 	status_user = status_dict['account']
 	user_id = status_user['id']
 	user_name = status_user['username']
-	if status_type == 'mention':
-		mastodon.status_post('Hey there, thanks for reaching out, '+user_name)
-	elif status_type != 'mention':
-		print ('No Mention')	
-		 		
-	
-    
+	status_msg_dict = status_dict['status']
+	status_msg = status_msg_dict['content']
+	#if status_type == 'mention':
+		#print (status_msg)
+	#elif status_type != 'mention':
+		#print ('No Mention')	
+	for x in statuses:
+		if status_type == 'mention':
+			print (status_msg)
+			plaintext = h2t(status_msg)
+			print (plaintext)
+		elif status_type != 'mention':
+			print ("No Content at post id:", status_id)
+			mastodon.notifications_dismiss(status_id)
+
+		
 fetch_notifications()
