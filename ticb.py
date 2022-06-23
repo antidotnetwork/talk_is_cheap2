74c828585bf635c0aada036d53aabadc434644c3
anti-dot-net <107893809+anti-dot-net@users.noreply.github.com>
2022-06-23 19:39:05 -0400

    Update ticb.py

--- ticb.py
+++ ticb.py
@@ -1,6 +1,7 @@
 from mastodon import Mastodon
-from bs4 import BeautifulSoup
 import re
+import dicttoxml
+import os
 
 mastodon = Mastodon(
   access_token = 'token.secret',
@@ -14,30 +15,51 @@ def h2t(raw_html):
   
 botself = mastodon.me()
 botself_id = botself["id"]
+print (botself_id)
+statuses = mastodon.timeline_local(limit=1)
+status_dict = statuses[0]
+# status_type = status_dict['type']
+status_id = status_dict['id']
+status_user = status_dict['account']
+#user_id = status_user['id']	
+user_name = status_user['username']
+#status_msg_dict = status_dict['status']
+status_msg = status_dict['content']
+plaintext = h2t(status_msg)
+xml_file = {'id' : status_id, 'username' : user_name, 'content' : plaintext}
+print (xml_file)
+xml_file = dicttoxml.dicttoxml(xml_file)
+print (xml_file)
+if 
+fetch_mostrecent()
 
-def fetch_notifications():
-	print (botself_id)
-	statuses = mastodon.notifications(limit=1)
-	status_dict = statuses[0]
-	status_type = status_dict['type']
-	status_id = status_dict['id']
-	status_user = status_dict['account']
-	user_id = status_user['id']
-	user_name = status_user['username']
-	status_msg_dict = status_dict['status']
-	status_msg = status_msg_dict['content']
-	#if status_type == 'mention':
-		#print (status_msg)
-	#elif status_type != 'mention':
-		#print ('No Mention')	
-	for x in statuses:
-		if status_type == 'mention':
-			print (status_msg)
-			plaintext = h2t(status_msg)
-			print (plaintext)
-		elif status_type != 'mention':
-			print ("No Content at post id:", status_id)
-			mastodon.notifications_dismiss(status_id)
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	#	if status_type == 'mention':
+	#		plaintext = h2t(status_msg)
+	#		print (plaintext)
+	#		tokens = nltk.word_tokenize(plaintext)
+	#		print (tokens)
+	#		for x in tokens:
+	#			print (x)
+	#			if x == 'Hello':
+	#				print ('Hi...')
+	#			elif x == '?':
+	#				print ('whats the question again?')
+	#	elif status_type != 'mention':
+	#		print ("No Content at post id:", status_id)
+	#		mastodon.notifications_dismiss(status_id)
 
 		
-fetch_notifications()
