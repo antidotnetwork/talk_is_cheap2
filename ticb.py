f1f317e9731159f41cf9eef47f903637ffddb0d4
anti-dot-net <107893809+anti-dot-net@users.noreply.github.com>
2022-06-23 21:11:06 -0400

    Update ticb.py

--- ticb.py
+++ ticb.py
@@ -12,26 +12,51 @@ CLEANR = re.compile('<.*?>')
 def h2t(raw_html):
   cleantext = re.sub(CLEANR, '', raw_html)
   return cleantext
-  
-botself = mastodon.me()
-botself_id = botself["id"]
-print (botself_id)
-statuses = mastodon.timeline_local(limit=1)
-status_dict = statuses[0]
-# status_type = status_dict['type']
-status_id = status_dict['id']
-status_user = status_dict['account']
-#user_id = status_user['id']	
-user_name = status_user['username']
-#status_msg_dict = status_dict['status']
-status_msg = status_dict['content']
-plaintext = h2t(status_msg)
-xml_file = {'id' : status_id, 'username' : user_name, 'content' : plaintext}
-print (xml_file)
-xml_file = dicttoxml.dicttoxml(xml_file)
-print (xml_file)
-if 
-fetch_mostrecent()
+
+
+def get_toot():  
+	botself = mastodon.me()
+	botself_id = botself["id"]
+	statuses = mastodon.timeline_local(limit=1)
+	status_dict = statuses[0]
+	status_id = status_dict['id']
+	status_user = status_dict['account']
+	user_name = status_user['username']
+	status_msg = status_dict['content']
+	plaintext = h2t(status_msg)
+	xml_file = {'id' : status_id, 'username' : user_name, 'content' : plaintext}
+	xml_file = dicttoxml.dicttoxml(xml_file)
+	return xml_file, status_id
+
+def check_toot():
+	XML, ID = get_toot()
+	print (XML)
+	with open("IDS.txt", 'r') as f:
+		w = f.read()
+		if ID == int(w):
+			toot_ok = False
+		else:
+			toot_ok = True
+	with open("IDS.txt",'w') as f:
+		f.write(str(ID)+'\n')
+		f.close()
+	return XML, toot_ok
+		
+
+def write_xml(xxx):
+	
+	print (XML)
+	with open("IDS.txt", 'r') as f:
+		w = f.read()
+		if ID == int(w):
+			toot_ok = False
+		else:
+			toot_ok = True
+	with open("IDS.txt",'w') as f:
+		f.write(str(ID)+'\n')
+		f.close()
+	return XML, toot_ok
+XML_File = check_toot()
 
 
 
@@ -61,5 +86,3 @@ fetch_mostrecent()
 	#	elif status_type != 'mention':
 	#		print ("No Content at post id:", status_id)
 	#		mastodon.notifications_dismiss(status_id)
-
-		
